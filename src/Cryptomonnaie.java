0bbf147c7eff4cdde450ec327195eb4748cd8fcb
Trystan Baillobay <baillobaytrystan06@gmail.com>
2023-04-11 09:34:20 +0200

    Ajout du fichier Cryptomonnaie.java

---
+++ src/Cryptomonnaie.java
@@ -0,0 +1,10 @@
+public class Cryptomonnaie{
+	private String nom;
+	private double valeurDeJeton; // Imaginons en euros
+
+	public Cryptomonnaie(String nom, double valeurDeJeton){
+		this.nom = nom;
+		this.valeurDeJeton = valeurDeJeton;
+	}
+}
+
